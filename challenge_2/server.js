e86da8e842630200744378e35845a92f7d846790
Phuc Tran <phuctranlv@gmail.com>
2020-03-03 11:24:32 -0800

    Create an express server to serve the client app

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const port = '3000';
+
+const pathToClient = path.join(__dirname, '/client/');
+
+app.use(express.static(pathToClient));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+})
 
