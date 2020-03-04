9f359ac2856a2df4a01994536615bc3380080dc9
Phuc Tran <phuctranlv@gmail.com>
2020-03-04 10:16:39 -0800

    include bodyParser to parse the data received by the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const process = require('./controller');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = '3000';
