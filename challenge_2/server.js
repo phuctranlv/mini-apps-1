70d88edf52730e952970a8f8a447679ffaced9cc
Phuc Tran <phuctranlv@gmail.com>
2020-03-03 17:13:21 -0800

    Include bodyParser as a middleware to parse data from form inputs

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const process = require('./controller');
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = '3000';
 
 const pathToClient = path.join(__dirname, '/client/');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  process(req, res);
+})
 
 app.use(express.static(pathToClient));
 
