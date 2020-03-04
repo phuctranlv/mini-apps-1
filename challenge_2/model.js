81b5294e031314d828bbebd33611ea4aa3f727c1
Phuc Tran <phuctranlv@gmail.com>
2020-03-03 17:16:59 -0800

    Create a model file to handle data processing

---
+++ challenge_2/model.js
@@ -0,0 +1,8 @@
+var dataConverter = function (data) {
+  if (data = 'hello') {
+    console.log('You got to the dataConverter part!');
+    return 'You got to the dataConverter part!';
+  }
+}
+
+module.exports = dataConverter;
