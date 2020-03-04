0753aab4f4bc08932bced90c41c0967eef5975d9
Phuc Tran <phuctranlv@gmail.com>
2020-03-04 10:15:37 -0800

    Create function to handle requets with the data from forms

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,21 @@
+var dataPackage = document.getElementById('data-submit-button');
 
+var sendData = function (form) {
+  const XHR = new XMLHttpRequest();
+  var FD = new FormData();
+  for (var name in form) {
+    FD.append(name, form[name]);
+  }
+  XHR.addEventListener('load', (event) => {
 
+  })
+  XHR.addEventListener('error', (event) => {
+
+  })
+  XHR.open('POST', 'http://localhost:3000/');
+  XHR.send(FD);
+}
+dataPackage.addEventListener('click', function () {
+  console.log(dataPackage);
+  sendData(dataPackage);
+})
